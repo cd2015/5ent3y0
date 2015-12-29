d3111a3f7122a07d79feddba336a01273c62fe99
emupuya <emupuya@cyberdynamics.co.ug>
2015-12-29 13:13:48 +0300

    Commented out links to online assets

--- views/footer.php
+++ views/footer.php
@@ -29,7 +29,7 @@
         </div>
     </div>
 </footer>
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script> -->
 <script>window.jQuery || document.write('<script src="/scripts/vendor/jquery-1.11.2.min.js"><\/script>')</script>
 <script src="/bower_components/retina.js/dist/retina.js"></script>
 <script src="/scripts/jquery.fancybox.pack.js"></script>
